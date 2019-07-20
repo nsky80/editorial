9a3867c58e8881d3ab79698a012cd796255fa7a9
Satish NSKY80 <satishkumary80@gmail.com>
2019-07-21 00:23:27 +0530

    Fixed login page problem

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -19,12 +19,12 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path('', views.homepage, name="homepage"),
-	path('logout/', views.logout_request, name="logout"),
-	path('login/', views.login_request, name="login"),
+	path('logout', views.logout_request, name="logout"),
+	path('login', views.login_request, name="login"),
 	path('register', views.register, name="register"),
 	path('about', views.about, name="about"),
 	path('community', views.community, name="community"),
-	path('account/', views.account, name="account"),
-	path('experiment/', views.experiment, name="experiment"),
-	path('<single_slug>', views.single_slug, name="single_slug"),
+	path('account', views.account, name="account"),
+	path('experiment', views.experiment, name="experiment"),
+	# path('<single_slug>', views.single_slug, name="single_slug"),
 ]
