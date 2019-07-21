bc678f28d33d963eb2282186ca0fe4bd76c1aac1
Satish NSKY80 <satishkumary80@gmail.com>
2019-07-21 22:34:46 +0530

    Configure TinyMCE editor

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Essay, EssaySeries, EssayCategory
+from tinymce.widgets import TinyMCE
+from django.db import models
 
+class EssayAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Title/date", {'fields': ["essay_title", "essay_published"]}),
+        ("Series/Slug", {'fields': ["series_title", "essay_slug"]}),
+        ("Content", {"fields": ["essay_content"]}),
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
 
 # Register your models here.
-admin.site.register(Essay)
+admin.site.register(Essay, EssayAdmin)    # this overrides default by above cofig
 admin.site.register(EssaySeries)
 admin.site.register(EssayCategory)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
 	'main.apps.MainConfig',
 ]
 
@@ -99,6 +100,38 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# Configurations of tinymce Editor
+# http://romanvm.github.io/django-tinymce4-lite/configuration.html
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'modern',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    }
 
 
 # Internationalization
